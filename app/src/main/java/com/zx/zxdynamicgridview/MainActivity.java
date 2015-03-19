9d37532c705185200816a6bf15bb06ca9cc852e6
赵新 <zhaoxin@souche.com>
2015-03-19 18:21:49 +0800

    修改sample。

--- app/src/main/java/com/zx/zxdynamicgridview/MainActivity.java
+++ app/src/main/java/com/zx/zxdynamicgridview/MainActivity.java
@@ -27,12 +27,12 @@ public class MainActivity extends ActionBarActivity {
     private ZXDynamicGridView dynamic_grid_view;
     private Integer[] picIds = {R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3, R.mipmap.pic4,
             R.mipmap.pic5, R.mipmap.pic6, R.mipmap.pic7, R.mipmap.pic8,
-            R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3, R.mipmap.pic4,
-            R.mipmap.pic5, R.mipmap.pic6, R.mipmap.pic7, R.mipmap.pic8,
-            R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3, R.mipmap.pic4,
-            R.mipmap.pic5, R.mipmap.pic6, R.mipmap.pic7, R.mipmap.pic8,
-            R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3, R.mipmap.pic4,
-            R.mipmap.pic5, R.mipmap.pic6, R.mipmap.pic7, R.mipmap.pic8};
+            R.mipmap.pic9, R.mipmap.pic10, R.mipmap.pic11, R.mipmap.pic12,
+            R.mipmap.pic13, R.mipmap.pic14, R.mipmap.pic15, R.mipmap.pic16,
+            R.mipmap.pic17, R.mipmap.pic18, R.mipmap.pic19, R.mipmap.pic20,
+            R.mipmap.pic21, R.mipmap.pic22, R.mipmap.pic23, R.mipmap.pic24,
+            R.mipmap.pic25, R.mipmap.pic26, R.mipmap.pic27, R.mipmap.pic28,
+            R.mipmap.pic29, R.mipmap.pic30};
     private List<Integer> picIdList = Arrays.asList(picIds);
     private TestAdapter testAdapter;
 
